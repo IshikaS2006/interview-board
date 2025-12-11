f96b9ab2260b294e56484642adbd62522debaa60
IshikaS2006 <ishika.singh24@st.niituniversity.in>
2025-12-11 23:51:55 +0530

    f

---
+++ server/test/test-user.js
@@ -0,0 +1,24 @@
+const { io } = require("socket.io-client");
+
+const socket = io("http://localhost:3001");
+
+socket.on("connect", () => {
+  console.log("Connected:", socket.id);
+
+  socket.emit("joinRoom", {
+    roomId: "ee08f170-162d-45d1-95b9-cc78433c0555",
+    userId: "user1",
+    adminKey: "a9d46c0a509835f1aaf83e89c60cb94b"
+  });
+});
+socket.on("initial-state", (data) => {
+  console.log("initial-state:", data);
+});
+
+socket.on("error", (err) => {
+  console.log("error:", err);
+});
+
+socket.on("initial-state", (msg) => console.log("initial", msg));
+socket.on("public-stroke", (msg) => console.log("public", msg));
+socket.on("private-stroke", (msg) => console.log("private", msg));
